2f02becd4d78f968ca16cbbbc75668e573794742
Alexandr <volokitin200@bk.ru>
2026-03-14 16:15:11 +0300

    fix: got rid of cos overflow error

--- src/main/kotlin/domain/Cosine.kt
+++ src/main/kotlin/domain/Cosine.kt
@@ -5,6 +5,6 @@ import java.math.BigDecimal
 
 class Cosine: MathFunction() {
     override fun invoke(x: BigDecimal, e: BigDecimal): BigDecimal {
-        return Sine()(x + MathConstants.MY_PI.value.divide(BigDecimal(2)), e)
+        return Sine()(MathConstants.MY_PI.value.divide(BigDecimal(2)) - x, e)
     }
 }
